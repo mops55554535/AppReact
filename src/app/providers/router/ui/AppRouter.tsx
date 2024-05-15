e141337a02458dcbdcdbb86b723629c0fdd8fc39
mops55554535 <banbanbanbanbanban5@gmail.com>
2024-05-15 18:26:33 +0300

    Non-existent routes. Loader for loading pages 	modified:   public/locales/en/translation.json 	modified:   src/app/providers/router/ui/AppRouter.tsx 	new file:   src/pages/NotFound/index.ts 	new file:   src/pages/NotFound/ui/NotFoundPage.module.scss 	new file:   src/pages/NotFound/ui/NotFoundPage.tsx 	modified:   src/shared/lib/routeConfid/routeConfid.tsx 	new file:   src/shared/ui/Loader/Loader.scss 	new file:   src/shared/ui/Loader/Loader.tsx 	new file:   src/shared/ui/PageLoader/PageLoader.module.scss 	new file:   src/shared/ui/PageLoader/PageLoader.tsx

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,25 +3,23 @@ import { MainPageAsync } from 'pages/MainPage'
 import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/lib/routeConfid/routeConfid'
-
-export default function  () {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({element, path})=>(
-
-              <Route
-               key={path}
-               path = {path}
-               element = {(
-              //element - компонент со страницей
-              <div className='page-wrapper'>
-                  {element}
-              </div>
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader'
+const AppRouter = () => (
+  <Routes>
+      {Object.values(routeConfig).map(({ element, path }) => (
+          <Route
+              key={path}
+              path={path}
+              element={(
+                  <Suspense fallback={<PageLoader />}>
+                      <div className="page-wrapper">
+                          {element}
+                      </div>
+                  </Suspense>
               )}
-              />
-            ))}
-        </Routes>
-    </Suspense>
-  )
-}
+          />
+      ))}
+  </Routes>
+);
+
+export default AppRouter;
--- src/shared/lib/routeConfid/routeConfid.tsx
+++ src/shared/lib/routeConfid/routeConfid.tsx
@@ -1,14 +1,17 @@
 import AboutPage from "pages/AboutPage/ui/AboutPage"
 import MainPage from "pages/MainPage/ui/MainPage"
+import { NotFoundPage } from "pages/NotFound"
 import { RouteProps } from "react-router-dom"
 
 export enum AppRoutes {
     MAIN = "main",
-    ABOUT = "about"
+    ABOUT = "about",
+    NotFoundPage = "not_found"
 }
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: "/",
-    [AppRoutes.ABOUT]: "/about"
+    [AppRoutes.ABOUT]: "/about",
+    [AppRoutes.NotFoundPage]: "*"
 }
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -19,5 +22,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage/>
+    },
+    [AppRoutes.NotFoundPage]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage/>
     }
 }
