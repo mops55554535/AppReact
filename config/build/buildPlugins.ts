7cf95d21407077f1da3e93a2ffa21026443844bd
mops55554535 <banbanbanbanbanban5@gmail.com>
2024-05-17 09:22:36 +0300

    BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,9 +1,12 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
-import {BuildOptions} from "./types/config";
+
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import {BuildOptions} from "./types/config";
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
+
 
     return [
         new HtmlWebpackPlugin({
@@ -17,6 +20,9 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new webpack.DefinePlugin({
             __IS__DEV__: JSON.stringify(isDev)
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        })
     ]
 }
