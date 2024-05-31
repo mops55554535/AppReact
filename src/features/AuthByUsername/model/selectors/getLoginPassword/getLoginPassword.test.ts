f48cf187777cf87e7f67d6d3fb67ecc22fc8cb51
Mops <banbanbanbanbanban5@gmail.com>
2024-05-31 11:52:13 +0300

    add test for login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginPassword/getLoginPassword.test.ts
@@ -0,0 +1,19 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+
+import { getLoginPassword } from './getLoginPassword';
+
+describe('getLoginPassword.test', () => {
+    test('should return value ', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                password: '1212121',
+            },
+        };
+        expect(getLoginPassword(state as StateSchema)).toEqual('1212121');
+    });
+    test('should return error ', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginPassword(state as StateSchema)).toEqual('');
+    });
+});
