8521a5ad539bf138547733925b120116c8e5545f
Mops <banbanbanbanbanban5@gmail.com>
2024-06-09 10:01:55 +0300

    add articles page 	modified:   config/build/buildWebpackConfig.ts 	new file:   src/pages/ArticleDetailsPage/index.tsx 	new file:   src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx 	new file:   src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.module.scss 	new file:   src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.tsx 	new file:   src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx 	new file:   src/pages/ArticlesPage/index.ts 	new file:   src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx 	new file:   src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.module.scss 	new file:   src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx 	new file:   src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx 	new file:   src/shared/assets/icons/article-20-20.svg 	new file:   src/shared/assets/icons/calendar-20-20.svg 	new file:   src/shared/assets/icons/copy-20-20.svg 	new file:   src/shared/assets/icons/done-20-20.svg 	new file:   src/shared/assets/icons/eye-20-20.svg 	modified:   src/shared/config/routeConfig/routeConfig.tsx 	modified:   src/widgets/Sidebar/model/items.ts

---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -0,0 +1,18 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './ArticlesPage.module.scss';
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+interface ArticlesPageProps {
+    className?: string;
+}
+
+const ArticlesPage = (props: ArticlesPageProps) => {
+    const { t } = useTranslation();
+const {className} = props
+    return (
+        <div className={classNames(cls.ArticlesPage, {}, [className])}>
+          Articles Page
+        </div>
+    );
+};
+export default memo(ArticlesPage)
