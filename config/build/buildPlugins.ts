f8d1d8e292f8457c428596995d1c41ade7ab2805
mops <banbanbanbanbanban5@gmail.com>
2024-06-30 14:22:10 +0300

    add circular dependency plugin 	modified:   config/build/buildPlugins.ts 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/entities/Country/ui/CountrySelect/CountrySelect.stories.tsx 	modified:   yarn.lock

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,6 +5,7 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
 import { BuildOptions } from './types/config';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 
 export function buildPlugins({
     paths, isDev, apiUrl, project,
@@ -28,6 +29,10 @@ export function buildPlugins({
                 { from: paths.locales, to: paths.buildLocales },
             ],
         }),
+        new CircularDependencyPlugin({
+            exclude: /node_modules/,
+            failOnError: true
+        })
     ];
 
     if (isDev) {
