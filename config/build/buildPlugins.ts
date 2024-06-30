214d8d10939f51446388a5e2ab35955ebce3751a
mops <banbanbanbanbanban5@gmail.com>
2024-06-30 15:10:21 +0300

    Migration to babel loader. Move type checking to a separate process. Writing our own babel plugin 	new file:   config/babel/babelRemovePropsPlugin.ts 	modified:   config/build/buildLoaders.ts 	modified:   config/build/buildPlugins.ts 	modified:   config/build/loaders/buildBabelLoader.ts 	new file:   extractedTranslations/ru/about.json 	new file:   extractedTranslations/ru/profile.json 	modified:   package-lock.json 	modified:   package.json 	modified:   yarn.lock

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,8 +4,9 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
-import { BuildOptions } from './types/config';
 import CircularDependencyPlugin from 'circular-dependency-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { BuildOptions } from './types/config';
 
 export function buildPlugins({
     paths, isDev, apiUrl, project,
@@ -31,8 +32,17 @@ export function buildPlugins({
         }),
         new CircularDependencyPlugin({
             exclude: /node_modules/,
-            failOnError: true
-        })
+            failOnError: true,
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            typescript: {
+                diagnosticOptions: {
+                    semantic: true,
+                    syntactic: true,
+                },
+                mode: 'write-references',
+            },
+        }),
     ];
 
     if (isDev) {
