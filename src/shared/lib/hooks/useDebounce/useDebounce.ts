29ddd43ff935ee5a76e1384feb882bf9200d708e
mops <banbanbanbanbanban5@gmail.com>
2024-07-05 13:38:37 +0300

    add js doc

--- src/shared/lib/hooks/useDebounce/useDebounce.ts
+++ src/shared/lib/hooks/useDebounce/useDebounce.ts
@@ -2,6 +2,11 @@ import { MutableRefObject, useCallback, useRef } from 'react';
 
 export function useDebounce(callback: (...args: any[]) => void, delay: number) {
     const timer = useRef() as MutableRefObject<any>;
+/**
+ * Хук, который позволяет отменять предыдущий вызов функции пока не истечет delay
+ * @param callback
+ * @param delay - задержка в мс
+ */
 
     return useCallback((...args: any[]) => {
         if (timer.current) {
