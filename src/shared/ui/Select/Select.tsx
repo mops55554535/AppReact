a94ad4f75fcc11ef739e60ee47ba62d848c3f801
mops <banbanbanbanbanban5@gmail.com>
2024-07-06 11:45:47 +0300

    Fix linter 	modified:   src/shared/ui/Select/Select.tsx

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, memo, useMemo } from 'react';
+import { ChangeEvent, useMemo } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Select.module.scss';
 
@@ -16,7 +16,7 @@ interface SelectProps<T extends string> {
     readonly?: boolean;
 }
 
-export const Select =<T extends string> (props: SelectProps<T>) => {
+export const Select = <T extends string> (props: SelectProps<T>) => {
     const {
         className,
         label,
