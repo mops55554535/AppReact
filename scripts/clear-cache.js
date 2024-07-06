13ca72aad54dff41ff4cf5ce9bdba33fb95cc7c1
mops <banbanbanbanbanban5@gmail.com>
2024-07-06 16:14:36 +0300

    Fix clear-cache script error

--- scripts/clear-cache.js
+++ scripts/clear-cache.js
@@ -1,6 +1,13 @@
 let fs = require('fs');
-fs.rm('./node_modules/.cache', { recursive:true }, err => {
-  
-   if(err) throw err; // не удалось удалить папку node_modules/.cache
-   console.log('Папка успешно удалена');
-});
+   fs.stat('/node_modules/.cache', function(err) {
+      if (!err) {
+         fs.rm('./node_modules/.cache', { recursive:true }, err => {
+            if(err){
+               console.log('не удалось удалить папку node_modules/.cache')
+            };
+         })
+      }
+      else if (err.code === 'ENOENT') {
+          console.log('папки node_modules/.cache нет');
+      }
+  })
