c8b1686289622ec29ef1c5f551888f7b6c787aac
mops <banbanbanbanbanban5@gmail.com>
2024-07-06 13:30:03 +0300

    refactoring entities 	modified:   src/entities/Article/index.ts 	new file:   src/features/ArticleSortSelector/index.ts 	renamed:    src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.module.scss -> src/features/ArticleSortSelector/ui/ArticleSortSelector/ArticleSortSelector.module.scss 	renamed:    src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.stories.tsx -> src/features/ArticleSortSelector/ui/ArticleSortSelector/ArticleSortSelector.stories.tsx 	renamed:    src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx -> src/features/ArticleSortSelector/ui/ArticleSortSelector/ArticleSortSelector.tsx 	new file:   src/features/ArticleTypeTabs/index.ts 	renamed:    src/entities/Article/ui/ArticleTypeTabs/ArticleTypeTabs.stories.tsx -> src/features/ArticleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.stories.tsx 	renamed:    src/entities/Article/ui/ArticleTypeTabs/ArticleTypeTabs.tsx -> src/features/ArticleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx 	new file:   src/features/ArticleViewSelector/index.ts 	new file:   src/features/ArticleViewSelector/model/slices/ArticleViewSelectorSlice.ts 	new file:   src/features/ArticleViewSelector/model/types/ArticleViewSelectorSchema.ts 	renamed:    src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.module.scss -> src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.module.scss 	renamed:    src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx -> src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx 	renamed:    src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx -> src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx 	modified:   src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx

--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -2,8 +2,10 @@ import { useTranslation } from 'react-i18next';
 import { memo, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import {
-    ArticleSortField, ArticleSortSelector, ArticleTypeTabs, ArticleView, ArticleViewSelector,
+    ArticleSortField,
     ArticleType,
+    ArticleView,
+
 } from '@/entities/Article';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
@@ -19,6 +21,9 @@ import {
     getArticlesPageView,
 } from '../../model/selectors/articlesPageSelectors';
 import { articlesPageActions } from '../../model/slices/articlesPageSlice';
+import { ArticleSortSelector } from '@/features/ArticleSortSelector';
+import { ArticleTypeTabs } from '@/features/ArticleTypeTabs';
+import { ArticleViewSelector } from '@/features/ArticleViewSelector';
 
 interface ArticlesPageFiltersProps {
     className?: string;
