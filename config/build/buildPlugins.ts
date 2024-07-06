631f7808826dbd1c04799000ecd729ebe6e58044
mops <banbanbanbanbanban5@gmail.com>
2024-07-06 14:50:54 +0300

    Fix linter 	modified:   config/build/buildPlugins.ts

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -12,18 +12,17 @@ export function buildPlugins({
     paths, isDev, apiUrl, project,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
     const isProd = !isDev;
+
     const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
-
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project),
         }),
-
         new CircularDependencyPlugin({
             exclude: /node_modules/,
             failOnError: true,
@@ -46,20 +45,17 @@ export function buildPlugins({
             openAnalyzer: false,
         }));
     }
+
     if (isProd) {
-        plugins.push(
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].[contenthash:8].css',
-                chunkFilename: 'css/[name].[contenthash:8].css',
-            }),
-        ),
-        plugins.push(
-            new CopyPlugin({
-                patterns: [
-                    { from: paths.locales, to: paths.buildLocales },
-                ],
-            }),
-        );
+        plugins.push(new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }));
+        plugins.push(new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales },
+            ],
+        }));
     }
 
     return plugins;
